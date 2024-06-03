6dbdc2321abe1140f074ba326f3fc61ed525e4dd
cauematheus23 <cauematheus_23@hotmail.com>
2024-06-03 16:07:37 -0300

    Add project files.

---
+++ 5by5-Dapper/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Controllers;
+using Models;
+
+Console.WriteLine("Inicio...");
+
+Pedido pedido = new Pedido  
+{
+    Id = 1,
+    Descricao = "Quero tudo",
+    Mesa = 10
+};
+if(new PedidoController().Inserir(pedido))
+{
+    Console.WriteLine("Inserido com sucesso");
+}
+else
+{
+    Console.WriteLine("Erro");
+}
---
+++ Repositories/PedidoRepository.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Models;
+
+namespace Repositories
+{
+    public class PedidoRepository : IPedidoRepository
+    {
+        private string Conn { get; set; }
+        public PedidoRepository()
+        {
+            Conn = "Data Source=127.0.0.1; Initial Catalog=Restaurante; User Id=sa; Password=SqlServer2019!; TrustServerCertificate=true;";
+        }
+
+        public bool Inserir(Pedido pedido)
+        {
+            var status = false;
+            using (var db = new SqlConnection(Conn))
+            {
+                db.Open();
+                db.Execute("INSERT INTO TB_PEDIDO (Descricao,Mesa) Values (@Descricao,@Mesa)",pedido);
+                status = true;
+            }
+            return status;
+        }
+    }
+}
